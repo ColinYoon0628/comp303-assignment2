118620713cb258bfb0e5943d77d742744a186e5f
Kunwoo Yoon <ykw0628@gmail.com>
2022-10-24 20:56:14 -0400

    added query for logged in student

--- KunwooMel_COMP303_Assigment2/src/main/java/com/example/demo/StudentController.java
+++ KunwooMel_COMP303_Assigment2/src/main/java/com/example/demo/StudentController.java
@@ -1,30 +1,36 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StudentController {
 	@Autowired
 	private StudentRepository studentRepo;
-	
+	Map<String, Object> model = new HashMap<String, Object>();
+
 	@RequestMapping("/") //http://localhost:8085/
 	public String home() 
 	{
 		return "index";
 	}
 	@PostMapping("/login")
-	public @ResponseBody String add(
+	public @ResponseBody String login(
 			@RequestParam("userName") String userName,  
 			@RequestParam("userPassword") String userPassword)
 	{
-//		Employee emp = new Employee(empId, empName, jobTitle,salary, depNo);
-//		empRepo.save(emp);
-		return "student is added";
+		Student loggedInStudent = new Student();
+		loggedInStudent = studentRepo.findByUserName(userName).get(0);
+		model.put("studentInfo", loggedInStudent);
+		return "sports";
 	}
 	
 	@RequestMapping("/signIn")
--- KunwooMel_COMP303_Assigment2/src/main/java/com/example/demo/StudentRepository.java
+++ KunwooMel_COMP303_Assigment2/src/main/java/com/example/demo/StudentRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Student, Integer>{
 
+public interface StudentRepository extends JpaRepository<Student, Integer>{
+    public List<Student> findByUserName(String username);
 }
